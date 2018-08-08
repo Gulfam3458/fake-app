a0078ddb0e5142caa1a35fe81799839e5d5821b1
gulfam3458 <muhammadg347@gmail.com>
2018-08-08 15:00:43 +0500

    Add a Static Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hello, world!"
+    render html: "hello, world! this is your best chance for reading and practical to learn thing how it
+works
+be care
+
+be serious
+be honest
+be precious
+be postive
+be careful
+avoid sleep
+avoid careless and everything avoid in life which wastage your time"
   end
 end
 
